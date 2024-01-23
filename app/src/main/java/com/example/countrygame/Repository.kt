302443554e5931d69682ad9e76d48b08609da02c
Jiří Gába <jiri.gaba@seznam.cz>
2024-01-23 10:16:09 +0100

    Úprava třídy Repository

--- app/src/main/java/com/example/countrygame/Repository.kt
+++ app/src/main/java/com/example/countrygame/Repository.kt
@@ -1,6 +1,17 @@
 package com.example.countrygame
 
 import com.example.countrygame.api.CountryApiService
+import com.example.countrygame.model.CountryInfo
 
 class Repository(private val apiService: CountryApiService) {
+    suspend fun getCountryInfo(
+        region: String, limit: String, pretty: Boolean) : CountryInfo? {
+        val response = apiService.getCountryInfo(region, limit, pretty)
+
+        if(response.isSuccessful){
+            return  response.body()
+        }else{
+            return  null
+        }
+    }
 }
